fb14c00e27078c605eca693a8f428256912cd5f0
Flynet-BDD <noel.theo@free.fr>
2023-01-19 22:31:15 +0100

    Mise en place Spring

---
+++ projetPerso/src/test/java/theo/projetPerso/ProjetPersoApplicationTests.java
@@ -0,0 +1,13 @@
+package theo.projetPerso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPersoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
